4c462e08b05662e82f4967032d25cefcd38ca1c2
Alexsandr Ryzhkov <alexsandr.ryzhkov04@gmail.com>
2024-12-18 21:22:26 +0300

    vff

--- Pract3/src/main/java/task2/Task2.java
+++ Pract3/src/main/java/task2/Task2.java
@@ -1,21 +1,43 @@
 package task2;
 
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Single;
-
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Task2 {
 
     public static void main(String[] args) {
-        Random rand = new Random();
+    }
 
-        Observable<Integer> randStream = Observable
-                .range(1, rand.nextInt(0,1000))
-                .map(i -> rand.nextInt(1000));
 
-        Single<Long> countStream = randStream.count();
+    public static void taskOnly500() {
+        Random random = new Random();
 
-        var _ = countStream.subscribe(System.out::println);
+        IntStream randomNumbers = random.ints(1000, 0, 1001);
+
+        randomNumbers
+                .filter(num -> num > 500)
+                .forEach(System.out::println);
+    }
+
+    public static void combineStream() {
+        Random random = new Random();
+
+        IntStream stream1 = random.ints(1000, 0, 10);
+
+        IntStream stream2 = random.ints(1000, 0, 10);
+
+        IntStream combinedStream = IntStream.concat(stream1, stream2);
+
+        combinedStream.forEach(System.out::print);
+    }
+
+    public static void firstFive() {
+        Random random = new Random();
+
+        IntStream randomNumbers = random.ints(10, 0, 1001);
+
+        randomNumbers
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
